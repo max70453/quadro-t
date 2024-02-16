65d60ec8bb45af4c2874a4465ac450e86ecd335b
max70453 <max70453@gmail.com>
2024-02-16 13:52:57 +0300

    delete pages, add form contacts, add scripts js and php

--- js/main.js
+++ js/main.js
@@ -85,6 +85,84 @@
 
         portfolioIsotope.isotope({filter: $(this).data('filter')});
     });
+
+
     
+    //send message
+    $( document ).ready(function() {
+        $.validator.setDefaults({
+            errorClass: 'text-danger'
+        })
+
+        let contactForm = $('.ajax-contact-form');
+
+        contactForm.validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 4
+                },
+                email: {
+                    required: true
+                },
+                tel: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Имя обязательно для заполнения",
+                    minlength: jQuery.validator.format("Необходимо ввести как минимум {0} символовы")
+                },
+                email: {
+                    required: "Email обязательн для заполнения"
+                },
+                tel: {
+                    required: "Телефон обязательн для заполнения"
+                }
+            },
+            submitHandler: function(form){
+                var str = $(form).serialize();
+                $.ajax({
+                    type: "POST",
+                    url: "http://localhost/contact.php",
+                    //headers: {  "Access-Control-Allow-Origin:": "*"},
+                    data: str,
+                    success: function(xml, textStatus, xhr){
+                        if(xhr.status === 200){
+                            $("#exampleModal").modal("show");
+                        }
+                    },
+                    error: function (jqXHR, exception) {
+                        if (jqXHR.status === 0) {
+                            alert('Not connect. Verify Network.');
+                        } else if (jqXHR.status == 404) {
+                            alert('Requested page not found (404).');
+                        } else if (jqXHR.status == 500) {
+                            alert('Internal Server Error (500).');
+                        } else if (exception === 'parsererror') {
+                            alert('Requested JSON parse failed.');
+                        } else if (exception === 'timeout') {
+                            alert('Time out error.');
+                        } else if (exception === 'abort') {
+                            alert('Ajax request aborted.');
+                        } else {
+                            alert('Uncaught Error. ' + jqXHR.responseText);
+                        }
+                    }
+                });
+                form.reset();
+                return false;
+            }
+        });
+
+    });
+
+    // active navmenu
+   $(".navbar-nav").find(".nav-item").on('click', (e) => {
+        $(".navbar-nav").find(".nav-item").removeClass('active');
+        $(e.currentTarget).addClass("active");
+    });
+
 })(jQuery);
 
